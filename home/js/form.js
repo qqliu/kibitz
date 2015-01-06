99cd5f45358c00cab3e197779cd24bb470c1da88
qqliu <qliu2011@gmail.com>
2015-01-06 18:21:29 -0500

    more changes--need to rewrite redundant code

--- home/js/form.js
+++ home/js/form.js
@@ -272,6 +272,178 @@ $(".next-opt-2").click(function(){
         });
 });
 
+$(".next-opt-2").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+        if ($("#item-based-button").hasClass('button-active') && $("#ratings-based-button").hasClass('button-active')) {
+            next_fs = $("#item-rating-based-recommender-option-1");
+        } else if ($("#item-based-button").hasClass('button-active')) {
+            next_fs = $("#item-based-recommender-option-1");   
+        } else if ($("#ratings-based-button").hasClass('button-active')) {
+            next_fs = $("#ratings-based-recommender-option-1");
+        } else {
+            next_fs = $("#option-1-form-3");
+        }
+        next_fs.show();
+        
+        current_fs.animate({opacity: 0}, {
+                    step: function(now, mx) {
+                            //as the opacity of current_fs reduces to 0 - stored in "now"
+                            //1. scale current_fs down to 80%
+                            scale = 1 - (1 - now) * 0.2;
+                            //2. bring next_fs from the right(50%)
+                            left = (now * 50)+"%";
+                            //3. increase opacity of next_fs to 1 as it moves in
+                            opacity = 1 - now;
+                            current_fs.css({'transform': 'scale('+scale+')'});
+                            next_fs.css({'left': left, 'opacity': opacity});
+                    }, 
+                    duration: 800, 
+                    complete: function(){
+                            current_fs.hide();
+                            animating = false;
+                    }, 
+                    //this comes from the custom easing plugin
+                    easing: 'easeInOutBack'
+        });
+});
+
+$(".next-opt-2-info").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+        if ($("#item-based-button-2").hasClass('button-active') && $("#ratings-based-button").hasClass('button-active')) {
+            next_fs = $("#item-rating-based-recommender-option-2");
+        } else if ($("#item-based-button-2").hasClass('button-active')) {
+            next_fs = $("#item-based-recommender-option-2");   
+        } else if ($("#ratings-based-button-2").hasClass('button-active')) {
+            next_fs = $("#ratings-based-recommender-option-2");
+        }
+        
+        next_fs.show();
+        
+        current_fs.animate({opacity: 0}, {
+                    step: function(now, mx) {
+                            //as the opacity of current_fs reduces to 0 - stored in "now"
+                            //1. scale current_fs down to 80%
+                            scale = 1 - (1 - now) * 0.2;
+                            //2. bring next_fs from the right(50%)
+                            left = (now * 50)+"%";
+                            //3. increase opacity of next_fs to 1 as it moves in
+                            opacity = 1 - now;
+                            current_fs.css({'transform': 'scale('+scale+')'});
+                            next_fs.css({'left': left, 'opacity': opacity});
+                    }, 
+                    duration: 800, 
+                    complete: function(){
+                            current_fs.hide();
+                            animating = false;
+                    }, 
+                    //this comes from the custom easing plugin
+                    easing: 'easeInOutBack'
+        });
+});
+
+$(".previous-opt-2-form").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $("#option-2-form-1");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".next-recommender-types-opt-2").click(function(){
+	if(animating) return false;
+	animating = true;
+        
+	$("#progressbar li").eq(3).addClass("active");
+	current_fs = $(this).parent();
+	next_fs = $("#option-2-form-1");
+        
+	next_fs.show();
+        
+        current_fs.animate({opacity: 0}, {
+                    step: function(now, mx) {
+                            //as the opacity of current_fs reduces to 0 - stored in "now"
+                            //1. scale current_fs down to 80%
+                            scale = 1 - (1 - now) * 0.2;
+                            //2. bring next_fs from the right(50%)
+                            left = (now * 50)+"%";
+                            //3. increase opacity of next_fs to 1 as it moves in
+                            opacity = 1 - now;
+                            current_fs.css({'transform': 'scale('+scale+')'});
+                            next_fs.css({'left': left, 'opacity': opacity});
+                    }, 
+                    duration: 800, 
+                    complete: function(){
+                            current_fs.hide();
+                            animating = false;
+                    }, 
+                    //this comes from the custom easing plugin
+                    easing: 'easeInOutBack'
+        });
+});
+
+$(".previous-opt-2-info").click(function(){
+	if(animating) return false;
+	animating = true;
+        
+	$("#progressbar li").eq(3).removeClass("active");
+	current_fs = $(this).parent();
+	previous_fs = $("#option-2-form");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
 function setColor(e) {
    var target = e.target,
        status = e.target.classList.contains('button-active');
