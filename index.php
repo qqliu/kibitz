fd2e6fba3800dbd1419ce788ec6ffd3a2064bbc7
qqliu <qliu2011@gmail.com>
2015-01-21 16:48:13 -0500

    more jars

--- index.php
+++ index.php
@@ -13,60 +13,114 @@
     require_once($GLOBALS['THRIFT_ROOT'].'/type/TType.php');
     require_once($GLOBALS['THRIFT_ROOT'].'/type/TMessageType.php');
     require_once($GLOBALS['THRIFT_ROOT'].'/RecommenderService.php');
-    
-    // Create a thrift connection
-    //$socket = new Thrift\Transport\TSocket('kibitz.csail.mit.edu', '9888');
-    $socket = new Thrift\Transport\TSocket('localhost', '9888');
-    $transport = new Thrift\Transport\TBufferedTransport($socket);
-    $protocol = new Thrift\Protocol\TBinaryProtocol($transport);
-    // Create a reposearch service client
-    $client = new kibitz\RecommenderServiceClient($protocol);
-    
-    // Open up the connection
-    $transport->open();
-    $client -> initiateModel('f6afe418118814ae1c62aeae803ab049');
-    
-    $recommendations = $client->makeRecommendation(999999, 10);
-    foreach($recommendations as $rec) {
-      echo implode(", ", $rec);
-    }
-    
-    /*$allitems = $client -> getItems();
-    foreach($allitems as $item) {
-      echo implode(", ", $item);
-    }*/
-    
-    $client -> createNewUser("quanquan", "quanquan@mit.edu", "HelloThere123!");
-    $user = $client -> recordRatings(999996, 1000, 10);
-    echo $user;
-    $login = $client -> checkLogin("quanquan", "HelloThere123!");
-    echo $login;
-    $login2 = $client -> checkLogin("quanquan", "HelloThere123");
-    echo $login2;
-    $myratings = $client -> getUserRatedItems(999999);
-    foreach($myratings as $rating) {
-      echo implode(", ", $rating);
-    }
 ?>
 
-<html> 
-<body>
-  <form enctype="multipart/form-data" action="upload_file.php" method="post">
-    Choose a file to upload: <input name="uploaded_file" type="file" />
-    <input type="submit" value="Upload" />
-  </form> 
-</body> 
-</html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <title>Kibitz</title>
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" type="text/css" rel="stylesheet">
 
-<!--<html>
-  <body>
+
+
+
+
+
+
+
+
+        <!-- CSS code from Bootply.com editor -->
+        
+        <style type="text/css">
+            html,body {
+  height:100%;
+}
+
+h1 {
+  font-family: Arial,sans-serif
+  font-size:80px;
+  color:#DDCCEE;
+}
+
+.lead {
+	color:#DDCCEE;
+}
+
+
+/* Custom container */
+.container-full {
+  margin: 0 auto;
+  width: 100%;
+  min-height:100%;
+  background-color:#110022;
+  color:#eee;
+  overflow:hidden;
+}
+
+.container-full a {
+  color:#efefef;
+  text-decoration:none;
+}
+
+.v-center {
+  margin-top:7%;
+}
   
-    <form action="upload_file.php" method="post"
-    enctype="multipart/form-data">
-      <label for="file">Filename:</label>
-      <input type="file" name="file" id="file"><br>
-      <input type="submit" name="submit" value="Submit">
-    </form>
+        </style>
+    </head>
+    
+    <!-- HTML code from Bootply.com editor -->
+    
+    <body  >
+        
+        <div class="container-full">
+
+      <div class="row">
+       
+        <div class="col-lg-12 text-center v-center">
+          
+          <h1>Kibitz</h1>
+          <p class="lead">Upload a file to <a href="\\datahub.csail.mit.edu">DataHub</a> and start recommending.</p>
+          
+          <br><br>
+          
+          <form class="col-lg-12" enctype="multipart/form-data" action="create_directory.php" method=""><br>
+            <h3>DataHub Username: </h3> <input style="margin-left:0%; margin-top:30px; margin-bottom:20px;" name="username" type="text" />
+            <h3>DataHub Password: </h3><input style="margin-top:30px; margin-bottom:30px;" name="password" type="password" /><br>
+            <h3>DataHub Repository: </h3><input style="margin-top:30px; margin-bottom:30px;" name="repo" type="text" /><br>
+            <h3>DataHub Table Name: </h3><input style="margin-top:30px; margin-bottom:30px;" name="tablename" type="text" /><br>
+            <input type="submit" value="Create" />
+          </form>
+        </div>
+        
+      </div> <!-- /row -->
   
-  </body>
-</html>-->
+  	  <div class="row">
+       
+        <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
+          <a href="https://github.com/qqliu/kibitz"><i class="icon-github"></i>
+        </div>
+      
+      </div>
+  
+  	<br><br><br><br><br>
+
+</div> <!-- /container full -->
+
+
+        
+        <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
+        <script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
--- upload_file.php
+++ upload_file.php
@@ -4,8 +4,35 @@ ini_set('memory_limit', '500M');
 
 if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0)) {
   //Check if the file is JPEG image and it's size is less than 350Kb
+  $dst =  $_POST['recommender_name'];
+  $url = "http://localhost/kibitz-demo/" . $dst;
+  $src = getcwd();
+  $dst = $src . "/" . $dst;
+  $src = $src . "/base";
+  mkdir($dst);
+  function recurse_copy($src,$dst) { 
+    $dir = opendir($src); 
+    @mkdir($dst); 
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir);
+  }
+  recurse_copy($src, $dst);
+  
   $filename = basename($_FILES['uploaded_file']['name']);
-  $databaseitemtable = "f6afe418118814ae1c62aeae803ab049";
+  $databaseitemtable = md5(uniqid(rand(), true));
+  
+  $content = '<?php $client -> initiateModel("' . $databaseitemtable . '"); $name = "' . $_POST['recommender_name'] . '";?>'; 
+  file_put_contents($dst . "/initiate_model.php", $content);
+  
   $databaseratingtable = $databaseitemtable . "ratings";
   $databaseusertable = $databaseitemtable . "users";
   $sql = "CREATE TABLE " . $databaseitemtable . " (id MEDIUMINT NOT NULL AUTO_INCREMENT, item VARCHAR(500), description VARCHAR(1000), image VARCHAR(500), primary key (id))";
@@ -13,19 +40,19 @@ if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0
   $usersql = "CREATE TABLE " . $databaseusertable . " (id MEDIUMINT NOT NULL AUTO_INCREMENT, username VARCHAR(100), email VARCHAR(100), password VARCHAR(500), primary key (id));";
   
   if (mysqli_query($con,$sql)) {
-    echo "Table " . $databaseitemtable . " created successfully";
+    //echo "Table " . $databaseitemtable . " created successfully";
   } else {
     echo "Error creating table: " . mysqli_error($con);
   }
   
   if (mysqli_query($con,$usertablesql)) {
-    echo "Table " . $databaseratingtable . " created successfully";
+    //echo "Table " . $databaseratingtable . " created successfully";
   } else {
     echo "Error creating table: " . mysqli_error($con);
   }
   
   if (mysqli_query($con,$usersql)) {
-    echo "Table " . $databaseusertable . " created successfully";
+    //echo "Table " . $databaseusertable . " created successfully";
   } else {
     echo "Error creating table: " . mysqli_error($con);
   }
@@ -37,7 +64,7 @@ if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0
       $allrows = array();
       foreach($rows as $key => $row) {
         if (count($allrows) > 1000) {
-            mysqli_query($con, "INSERT INTO $databaseratingtable (user_id, item_id, rating) VALUES " . implode(', ', $allrows) . ";") or die(mysqli_error($con));
+            mysqli_query($con, "INSERT INTO $databaseitemtable (item, description, image) VALUES " . implode(', ', $allrows) . ";"); //or die(mysqli_error($con));
             unset($allrows);
             $allrows = array();
         }
@@ -54,7 +81,7 @@ if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0
       }
       //echo implode(', ', $allrows);
       //echo "INSERT INTO $databaseitemtable (item, description, image) VALUES " . implode(', ', $allrows) . ";";
-      mysqli_query($con, "INSERT INTO $databaseitemtable (item, description, image) VALUES " . implode(', ', $allrows) . ";") or die(mysqli_error($con));
+      mysqli_query($con, "INSERT INTO $databaseitemtable (item, description, image) VALUES " . implode(', ', $allrows) . ";"); //or die(mysqli_error($con));
       mysqli_close($con);
   } else {
      echo "Error: Only .txt and .csv files under 350Kb are accepted for upload";
@@ -63,3 +90,103 @@ if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0
  echo "Error: No file uploaded";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <title>Kibitz</title>
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+
+
+
+
+
+
+
+
+
+        <!-- CSS code from Bootply.com editor -->
+        
+        <style type="text/css">
+            html,body {
+  height:100%;
+}
+
+h1 {
+  font-family: Arial,sans-serif
+  font-size:80px;
+  color:#DDCCEE;
+}
+
+.lead {
+	color:#DDCCEE;
+}
+
+
+/* Custom container */
+.container-full {
+  margin: 0 auto;
+  width: 100%;
+  min-height:100%;
+  background-color:#110022;
+  color:#eee;
+  overflow:hidden;
+}
+
+.container-full a {
+  color:#efefef;
+  text-decoration:none;
+}
+
+.v-center {
+  margin-top:7%;
+}
+  
+        </style>
+    </head>
+    
+    <!-- HTML code from Bootply.com editor -->
+    
+    <body  >
+        
+        <div class="container-full">
+
+      <div class="row">
+       
+        <div class="col-lg-12 text-center v-center">
+          
+          <h1>Kibitz</h1>
+          <p class="lead">Your recommender is located at <a href= "<?php echo $url; ?>" ><?php echo $url; ?></a>.</p>
+        </div>
+        
+      </div> <!-- /row -->
+  
+  	  <div class="row">
+       
+        <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
+          <a href="https://github.com/qqliu/kibitz"><i class="icon-github"></i>
+        </div>
+      
+      </div>
+  
+  	<br><br><br><br><br>
+
+</div> <!-- /container full -->
+
+
+        
+        <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
+        <script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
