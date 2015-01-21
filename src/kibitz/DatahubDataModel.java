9e1ec5aeb46a8fc8a038d2970ba8b03433c9957a
qqliu <qliu2011@gmail.com>
2015-01-21 17:21:43 -0500

    updated packages

--- src/kibitz/DatahubDataModel.java
+++ src/kibitz/DatahubDataModel.java
@@ -15,8 +15,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
