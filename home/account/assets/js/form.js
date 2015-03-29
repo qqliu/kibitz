8c55aeae9e63dba2b76f5071bce483c777173c9d
qqliu <qliu2011@gmail.com>
2015-03-29 19:10:30 -0400

    added complete frontendlogin functionality

--- home/account/assets/js/form.js
+++ home/account/assets/js/form.js
@@ -459,6 +459,56 @@ $(".submit").click(function(){
     return false;
 });
 
+var checkLogin = function() {
+    if ((sessionStorage.getItem("username") === null || sessionStorage.getItem("username") === "null" ||
+	 sessionStorage.getItem("username") === "undefined" || sessionStorage.getItem("username") === undefined)
+	    && (getCookie("username") === null || getCookie("username") === "null" ||
+	    getCookie("username") === "undefined" || getCookie("username") === undefined)) {
+	return true;
+    } else {
+	return false;
+    }
+};
+
+function setCookie(cname, cvalue, exdays) {
+    if (exdays !== null) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+    } else {
+	document.cookie = cname + "=" + cvalue + "; ";
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+} 
+
+function deleteLoginInfo() {
+    setCookie("username", null);
+    sessionStorage.setItem("username", null);
+    document.location.href="..";
+}
+
 $(function() {
-    $( "#dialog" ).dialog();
+    if (checkLogin()) {
+	document.location.href = "..";
+    } else {
+	var username;
+	if (!(sessionStorage.getItem("username") === null || sessionStorage.getItem("username") === "null" ||
+	 sessionStorage.getItem("username") === "undefined" || sessionStorage.getItem("username") === undefined))
+	    username = sessionStorage.getItem("username")
+	else if (!(getCookie("username") === null || getCookie("username") === "null" ||
+	    getCookie("username") === "undefined" || getCookie("username") === undefined))
+	    username = getCookie("username");
+	$("#username_display").html(username);
+    }
 });
